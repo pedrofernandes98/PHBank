6e013517d644e49fb18dea93d1d56f22ff5b1fdc
pedrofernandes98 <ppedrohfs@gmail.com>
2021-04-23 01:23:57 -0300

    Criação de Projeto de Testes - Testes no método sacar

---
+++ PHBank_GerenciamentoContasTests/ContaCorrenteTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit.Sdk;
+
+namespace PHBank_GerenciamentoContas.Tests
+{
+    [TestClass()]
+    public class ContaCorrenteTests
+    {
+        [TestMethod()]
+        public void SacarSuccesTest()
+        {
+            #region Arrange
+            ContaCorrente contaCorrente = new ContaCorrente(1234, 123456789);
+            bool sucess = false;
+            contaCorrente.Depositar(1000);
+            #endregion
+
+            #region Act
+            sucess = contaCorrente.Sacar(100);
+            #endregion
+
+            #region Assert
+            Assert.IsTrue(sucess);
+            #endregion
+        }
+
+        [TestMethod()]
+        public void SacarFailTest()
+        {
+            #region Arrange
+            ContaCorrente contaCorrente = new ContaCorrente(1234, 123456789);
+            bool sucess = false;
+            contaCorrente.Depositar(1000);
+            #endregion
+
+            #region Act
+            sucess = contaCorrente.Sacar(10000);
+            #endregion
+
+            #region Assert
+            Assert.IsFalse(sucess);
+            #endregion
+        }
+
+        [TestMethod()]
+        public void SacarInvalidValueTest()
+        {
+            #region Arrange
+            ContaCorrente contaCorrente = new ContaCorrente(1234, 123456789);
+            bool sucess = false;
+            contaCorrente.Depositar(1000);
+            #endregion
+
+            #region Act
+            sucess = contaCorrente.Sacar(-40);
+            #endregion
+
+            #region Assert
+            Assert.IsFalse(sucess);
+            #endregion
+        }
+    }
+}
